e7cd73488bc537d2ec6aae3f6b4e570e9c2b571a
upasanaRaghav <raghavupasana44@gmail.com>
2024-03-30 00:35:28 +0530

    Initial

---
+++ src/assets/index.js
@@ -0,0 +1,88 @@
+import logo from "./lo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import webdev from "./tech/webdev.png";
+import workhome from "./tech/workhome.png";
+import chatgpt from "./tech/chatgpt.png";
+import c from "./tech/c.png";
+import coding from "./tech/coding.png";
+import bootstrap from "./tech/bootstrap.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import ai from "./company/ai.png";
+import cse from "./company/cse.png";
+import twelth from "./company/twelth.png";
+import tenth from "./company/tenth.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import wewatch from "./wewatch.png";
+import threeJS from "./threeJS.png";
+import chatbot from "./chatbot.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  ai,
+  webdev,
+  workhome,
+  chatgpt,
+  c,
+  coding,
+  bootstrap,
+  wewatch,
+  threeJS,
+  chatbot,
+  cse,
+  twelth,
+  tenth,
+};
